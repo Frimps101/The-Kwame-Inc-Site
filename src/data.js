2c43c954ec81fc8f0e4babbc371c8039ba8d8596
Josephine Frimpomaa Kwakye <jkwakye001@gmail.com>
2023-05-04 12:43:09 +0000

    Committing

--- src/data.js
+++ src/data.js
@@ -2,8 +2,8 @@ export const data = [
   {
     id: 1,
     img: "../images/collage.png",
-    title: "FRAME ANYTHING",
-    desc: "Whether it’s something from your closet or a photo on your phone, we can frame it",
+    title: "FRAME YOUR WORLD",
+    desc: "Transform Your Memories into Timeless Treasures!",
   }
 ];
 
